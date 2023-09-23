8f0c16bf4539cc435bf2e20b530ab9b57f8945fe
Kleo <mmttdd16@gmail.com>
2023-09-23 21:37:59 +0300

    Finished Ex1v2

--- Ex1v2/Program.cs
+++ Ex1v2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int WorkWithUser(string message)
+{
+    System.Console.Write(message);
+    int number = int.Parse(System.Console.ReadLine()!);
+    return number;
+}
+int FindSum(int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int num = WorkWithUser("Введите число A: ");
+System.Console.Write($"Сумма всех чисел от 1 до {num} равна {FindSum(num)}");
