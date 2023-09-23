e1912dea06bd55b206b8cc0b2a1147a1516f7600
Kleo <mmttdd16@gmail.com>
2023-09-23 21:25:48 +0300

    Finished Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,22 @@
 ﻿System.Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+if (num >= 0)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        // Добавляем текущее значение i к сумме.
+        sum += i;
+    }
+}
+else
+{
+    for (int i = 1; i >= num; i--)
+    {
+        // Добавляем текущее значение i к сумме.
+        sum += i;
+    }
+}
+
+System.Console.WriteLine("Сумма всех чисел от 1 до " + num + " равна " + sum);
