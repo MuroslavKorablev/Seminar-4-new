fc7d4b190ca566bb0c2dca9f5a4ebfe03b58c225
Kleo <mmttdd16@gmail.com>
2023-09-24 09:03:36 +0300

    Finished Ex1v2

--- Ex1v2/Program.cs
+++ Ex1v2/Program.cs
@@ -1,4 +1,6 @@
-﻿int WorkWithUser(string message)
+﻿using System.Formats.Asn1;
+
+int WorkWithUser(string message)
 {
     System.Console.Write(message);
     int number = int.Parse(System.Console.ReadLine()!);
@@ -14,5 +16,14 @@ int FindSum(int number)
     return sum;
 }
 
-int num = WorkWithUser("Введите число A: ");
-System.Console.Write($"Сумма всех чисел от 1 до {num} равна {FindSum(num)}");
+int number1 = WorkWithUser("Введите число A: ");
+int summ1 = FindSum(number1);
+int number2 = WorkWithUser("Введите число B: ");
+int summ2 = FindSum(number2);
+int number3 = WorkWithUser("Введите число C: ");
+int summ3 = FindSum(number3);
+
+
+Console.Write($"{summ1} + {summ2} + {summ3} = {summ1 + summ2 + summ3}");
+// Console.Write($"Сумма всех чисел от 1 до {number} равна {FindSum(number)}");
+
