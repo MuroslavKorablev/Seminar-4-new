fe9349fc1c1e83bfe6a082fe8ea8259457ae82e8
Kleo <mmttdd16@gmail.com>
2023-09-24 12:01:57 +0300

    Finished Ex3

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -4,3 +4,18 @@
     int number = int.Parse(System.Console.ReadLine()!);
     return number;
 }
+
+int Factorial(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result *= i; // Умножаем result на текущее значение i.
+    }
+    return result;
+}
+
+int number = WorkWithUser("Введите число: ");
+int result = Factorial(number);
+
+System.Console.WriteLine($"Факториал {number} = {result}");
