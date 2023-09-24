90788148fa9b8fe99c611d4b959e4ee980be52fd
Kleo <mmttdd16@gmail.com>
2023-09-24 12:36:06 +0300

    Finished Ex4

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int[] array = new int[8];
+//  Random random = new Random();
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = random.Next(0, 2);
+//     System.Console.Write(array[i] + " ");    
+// }
+
+// Заполнение и вывод массива из 8 элементов случайными числами 0 и 1
+int[] numbers = new int[8];
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+} 
+
+void PrintArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        System.Console.Write(collection[i] + " ");
+    }
+}
