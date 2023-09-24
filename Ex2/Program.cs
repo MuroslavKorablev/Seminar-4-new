a7755dc7abb682a7a69d89be718289c3cd37d34a
Kleo <mmttdd16@gmail.com>
2023-09-24 10:33:20 +0300

    Create and Finished Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿int WorkWithUser(string message)
+{
+    System.Console.Write(message);
+    int number = int.Parse(System.Console.ReadLine()!);
+    return number;
+}
+
+int countDigit(int number)
+{
+    int count = 0;
+    while (number != 0)
+{
+    count++;
+    number = number / 10;
+}
+    return count;
+}
+
+int number = WorkWithUser("Введите число: ");
+int count = countDigit(number);
+System.Console.WriteLine($"В числе {number} количество цифр равно: {count}");
